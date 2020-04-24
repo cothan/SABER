eef3c666f7bb3ec1347e83cca6dd5f0f9238716d
cothan <cothannguyen@gmail.com>
2020-04-24 17:33:38 -0400

    update

--- Cortex-A/polymul/toom_cook_4/asimd_toom-cook_4way.c
+++ Cortex-A/polymul/toom_cook_4/asimd_toom-cook_4way.c
@@ -78,21 +78,21 @@ void karatsuba32_fork_avx_new(uint16x8x2_t *a1,
 							  uint16x8x2_t *b1,
 							  uint8_t position)
 {
-	COPYDOT(a[position], a1[0]);
-	COPYDOT(b[position], b1[0]);
+	COPYDOT_DOT(a[position], a1[0]);
+	COPYDOT_DOT(b[position], b1[0]);
 
 	if ((position + 1) > 15)
 	{
-		COPYDOT(a_extra[0] , a1[1]);
-		COPYDOT(b_extra[0] , b1[1]);
+		COPYDOT_DOT(a_extra[0] , a1[1]);
+		COPYDOT_DOT(b_extra[0] , b1[1]);
 
 		ADDDOT(a_extra[1], a1[0], a1[1]);
 		ADDDOT(b_extra[1], b1[0], b1[1]);
 	}
 	else
 	{
-		COPYDOT(a[position + 1] , a1[1]);
-		COPYDOT(b[position + 1] , b1[1]);
+		COPYDOT_DOT(a[position + 1] , a1[1]);
+		COPYDOT_DOT(b[position + 1] , b1[1]);
 
 		ADDDOT(a[position + 2], a1[0], a1[1]);
 		ADDDOT(b[position + 2], b1[0], b1[1]);
@@ -103,8 +103,8 @@ void karatsuba32_fork_avx_partial(uint16x8x2_t *a1,
 								  uint16x8x2_t *b1,
 								  uint8_t position)
 {
-	COPYDOT(a[position], a1[1]);
-	COPYDOT(b[position], b1[1]);
+	COPYDOT_DOT(a[position], a1[1]);
+	COPYDOT_DOT(b[position], b1[1]);
 
 	ADDDOT(a[position + 1], a1[0], a1[1]);
 	ADDDOT(b[position + 1], b1[0], b1[1]);
