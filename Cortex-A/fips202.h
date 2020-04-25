029e0ce3f4ac3ab260e8490c4dad3ddfd96256af
cothan <cothannguyen@gmail.com>
2020-04-25 00:45:31 -0400

    add sha3, focus on sha3 ASIMD

--- Cortex-A/fips202.h
+++ Cortex-A/fips202.h
@@ -8,11 +8,22 @@
 #define SHA3_256_RATE 136
 #define SHA3_512_RATE 72
 
-void shake128(unsigned char *output, unsigned long long outlen, const unsigned char *input,  unsigned long long inlen);
+#if __aarch64__
+#include <libkeccak.a.headers/SimpleFIPS202.h>
+#else
+#error "Install SIMD Keccak"
+#endif
 
-//void shake128(unsigned char *output, unsigned long long outlen, const unsigned char *input,  unsigned long long inlen);
-void sha3_256(unsigned char *output, const unsigned char *input,  unsigned long long inlen);
-void sha3_512(unsigned char *output, const unsigned char *input,  unsigned long long inlen);
+#define shake128 SHAKE128
+#define sha3_256 SHA3_256
+#define sha3_512 SHA3_512
+
+
+// void shake128(unsigned char *output, unsigned long long outlen, const unsigned char *input,  unsigned long long inlen);
+
+// //void shake128(unsigned char *output, unsigned long long outlen, const unsigned char *input,  unsigned long long inlen);
+// void sha3_256(unsigned char *output, const unsigned char *input,  unsigned long long inlen);
+// void sha3_512(unsigned char *output, const unsigned char *input,  unsigned long long inlen);
 
 void cshake128_simple(unsigned char *output, unsigned long long outlen, 
                       uint16_t cstm, // 2-byte domain separator
