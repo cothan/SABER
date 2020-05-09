6c9de4ad2d40780c1744e0ce41631ebffddb930e
cothan <cothannguyen@gmail.com>
2020-05-09 07:22:45 -0400

    rename

--- Cortex-A/polymul/toom_cook_4/toom_cook_4way_neon.c
+++ Cortex-A/polymul/toom_cook_4/toom_cook_4way_neon.c
@@ -73,13 +73,13 @@ void vxor(uint16x8x2_t c, uint16x8x2_t a, uint16x8x2_t b)
 
 
 /*
- * toom_cook_4way_avx
+ * toom_cook_4way_neon
  * Main function 
  * a1_avx: In memory [256]
  * b1_avx: In memory  [256]
  * res_avx_output: In memory [512]
  */
-void toom_cook_4way_avx(uint16_t *a1_avx,
+void toom_cook_4way_neon(uint16_t *a1_avx,
 						uint16_t *b1_avx,
 						uint16_t p_mod,
 						uint16_t *res_avx_output)
