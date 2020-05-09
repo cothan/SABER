a7212ed0f82f662a06ec296b0800c967820804ac
cothan <cothannguyen@gmail.com>
2020-05-09 07:15:38 -0400

    finish school book

---
+++ Cortex-A/polymul/toom_cook_4/asimd_scm_neon.h
@@ -0,0 +1,13 @@
+#ifndef ASIMD_SCM_NEON
+#define ASIMD_SCM_NEON 
+
+#define SCM_SIZE 16
+
+uint16_t c_avx[16 * 2 * SCM_SIZE];
+uint16_t a[16 * SCM_SIZE];
+uint16_t b[16 * SCM_SIZE];
+uint16_t c_avx_extra[16 * 4];
+
+void schoolbook_neon_new(uint16_t *c, uint16_t *a_mem, uint16_t *b_mem);
+
+#endif
