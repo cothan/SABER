dd1b21c99749f9b22c9182c421b6c37f42148fbb
cothan <cothannguyen@gmail.com>
2020-05-09 05:23:46 -0400

    finish

--- Cortex-A/polymul/toom_cook_4/asimd_matrix.c
+++ Cortex-A/polymul/toom_cook_4/asimd_matrix.c
@@ -1,32 +1,43 @@
-#define PERMUTE0(c, a, b, value)                                                                   \
-  c.val[0] = (uint16x8_t)a.val[0];                                                                \
-  c.val[1] = (uint16x8_t)b.val[0]
+static inline 
+void permute0(uint16_t *c, uint16x8x2_t a, uint16x8x2_t b, int16_t value)
+{
+  (void) value;
+  vst1q_u16(c, a.val[0]);
+  vst1q_u16(c + 8, b.val[0]);
+}
 
-#define PERMUTE1(c, a, b, value)                                                                          \
-  c.val[0] = (uint16x8_t)a.val[1];                                                                \
-  c.val[1] = (uint16x8_t)b.val[1]
+static inline 
+void permute1(uint16_t *c, uint16x8x2_t a, uint16x8x2_t b, int16_t value)
+{
+  (void) value;
+  vst1q_u16(c, a.val[1]);
+  vst1q_u16(c + 8, b.val[1]);
+}
 
 /*
- * tranpose 
+ * tranpose
  * Input: Memory uint16_t *M
  * Output: Memory uint16_t *M
  */
-static inline 
-void transpose(uint16x8x2_t *M) {
+void transpose(uint16_t *M) {
   int i;
   uint16x8x2_t tL[8], tH[8];
   uint32x4x2_t bL[4], bH[4], cL[4], cH[4];
   uint64x2x2_t dL[2], dH[2], eL[2], eH[2], fL[2], fH[2], gL[2], gH[2];
 
-#pragma clang loop unroll(full)
+  uint16x8x2_t aa, bb;
+
   for (i = 0; i < 8; i = i + 1) {
-    tL[i].val[0] = vzip2q_u16(M[2 * i].val[0], M[2 * i + 1].val[0]);
-    tL[i].val[1] = vzip1q_u16(M[2 * i].val[0], M[2 * i + 1].val[0]);
-    tH[i].val[0] = vzip2q_u16(M[2 * i].val[1], M[2 * i + 1].val[1]);
-    tH[i].val[1] = vzip1q_u16(M[2 * i].val[1], M[2 * i + 1].val[1]);
+    aa = vld2q_u16(&M[(2 * i) * 16]);
+    bb = vld2q_u16(&M[(2 * i + 1) * 16]);
+
+    tL[i].val[0] = vzip2q_u16(aa.val[0], bb.val[0]);
+    tL[i].val[1] = vzip1q_u16(aa.val[0], bb.val[0]);
+
+    tH[i].val[0] = vzip2q_u16(aa.val[1], bb.val[1]);
+    tH[i].val[1] = vzip1q_u16(aa.val[1], bb.val[1]);
   }
-  //-----------------------------------
-  #pragma clang loop unroll(full)
+//-----------------------------------
   for (i = 0; i < 4; i = i + 1) {
     bL[i].val[0] = vzip2q_u32((uint32x4_t)tL[2 * i].val[0], (uint32x4_t)tL[2 * i + 1].val[0]);
     bL[i].val[1] = vzip1q_u32((uint32x4_t)tL[2 * i].val[0], (uint32x4_t)tL[2 * i + 1].val[0]);
@@ -34,8 +45,7 @@ void transpose(uint16x8x2_t *M) {
     bH[i].val[0] = vzip2q_u32((uint32x4_t)tL[2 * i].val[1], (uint32x4_t)tL[2 * i + 1].val[1]);
     bH[i].val[1] = vzip1q_u32((uint32x4_t)tL[2 * i].val[1], (uint32x4_t)tL[2 * i + 1].val[1]);
   }
-  //-----------------------------------
-  #pragma clang loop unroll(full)
+//-----------------------------------
   for (i = 0; i < 4; i = i + 1) {
     cL[i].val[0] = vzip2q_u32((uint32x4_t)tH[2 * i].val[0], (uint32x4_t)tH[2 * i + 1].val[0]);
     cL[i].val[1] = vzip1q_u32((uint32x4_t)tH[2 * i].val[0], (uint32x4_t)tH[2 * i + 1].val[0]);
@@ -43,8 +53,7 @@ void transpose(uint16x8x2_t *M) {
     cH[i].val[0] = vzip2q_u32((uint32x4_t)tH[2 * i].val[1], (uint32x4_t)tH[2 * i + 1].val[1]);
     cH[i].val[1] = vzip1q_u32((uint32x4_t)tH[2 * i].val[1], (uint32x4_t)tH[2 * i + 1].val[1]);
   }
-  //------------------------------------
-  #pragma clang loop unroll(full)
+//------------------------------------
   for (i = 0; i < 2; i = i + 1) {
     dL[i].val[0] = vzip2q_u64((uint64x2_t)bL[2 * i].val[0], (uint64x2_t)bL[2 * i + 1].val[0]);
     dL[i].val[1] = vzip1q_u64((uint64x2_t)bL[2 * i].val[0], (uint64x2_t)bL[2 * i + 1].val[0]);
@@ -52,8 +61,11 @@ void transpose(uint16x8x2_t *M) {
     dH[i].val[0] = vzip2q_u64((uint64x2_t)bL[2 * i].val[1], (uint64x2_t)bL[2 * i + 1].val[1]);
     dH[i].val[1] = vzip1q_u64((uint64x2_t)bL[2 * i].val[1], (uint64x2_t)bL[2 * i + 1].val[1]);
   }
-  //------------------------------------
-  #pragma clang loop unroll(full)
+  permute0(&M[0*16], dL[0], dL[1], 0x20);
+  permute1(&M[8*16], dL[0], dL[1], 0x31);
+  permute0(&M[1*16], dH[0], dH[1], 0x20);
+  permute1(&M[9*16], dH[0], dH[1], 0x31);
+//------------------------------------
   for (i = 0; i < 2; i = i + 1) {
     eL[i].val[0] = vzip2q_u64((uint64x2_t)bH[2 * i].val[0], (uint64x2_t)bH[2 * i + 1].val[0]);
     eL[i].val[1] = vzip1q_u64((uint64x2_t)bH[2 * i].val[0], (uint64x2_t)bH[2 * i + 1].val[0]);
@@ -61,8 +73,11 @@ void transpose(uint16x8x2_t *M) {
     eH[i].val[0] = vzip2q_u64((uint64x2_t)bH[2 * i].val[1], (uint64x2_t)bH[2 * i + 1].val[1]);
     eH[i].val[1] = vzip1q_u64((uint64x2_t)bH[2 * i].val[1], (uint64x2_t)bH[2 * i + 1].val[1]);
   }
-  //-------------------------------------
-  #pragma clang loop unroll(full)
+  permute0(&M[16*2], eL[0], eL[1], 0x20);
+  permute1(&M[16*10], eL[0], eL[1], 0x31);
+  permute0(&M[16*3], eH[0], eH[1], 0x20);
+  permute1(&M[16*11], eH[0], eH[1], 0x31);
+//-------------------------------------
   for (i = 0; i < 2; i = i + 1) {
     fL[i].val[0] = vzip2q_u64((uint64x2_t)cL[2 * i].val[0], (uint64x2_t)cL[2 * i + 1].val[0]);
     fL[i].val[1] = vzip1q_u64((uint64x2_t)cL[2 * i].val[0], (uint64x2_t)cL[2 * i + 1].val[0]);
@@ -70,7 +85,11 @@ void transpose(uint16x8x2_t *M) {
     fH[i].val[0] = vzip2q_u64((uint64x2_t)cL[2 * i].val[1], (uint64x2_t)cL[2 * i + 1].val[1]);
     fH[i].val[1] = vzip1q_u64((uint64x2_t)cL[2 * i].val[1], (uint64x2_t)cL[2 * i + 1].val[1]);
   }
-  #pragma clang loop unroll(full)
+  permute0(&M[16*4], fL[0], fL[1], 0x20);
+  permute1(&M[16*12], fL[0], fL[1], 0x31);
+  permute0(&M[16*5], fH[0], fH[1], 0x20);
+  permute1(&M[16*13], fH[0], fH[1], 0x31);
+  //-------------------------------------
   for (i = 0; i < 2; i = i + 1) {
     gL[i].val[0] = vzip2q_u64((uint64x2_t)cH[2 * i].val[0], (uint64x2_t)cH[2 * i + 1].val[0]);
     gL[i].val[1] = vzip1q_u64((uint64x2_t)cH[2 * i].val[0], (uint64x2_t)cH[2 * i + 1].val[0]);
@@ -78,23 +97,9 @@ void transpose(uint16x8x2_t *M) {
     gH[i].val[0] = vzip2q_u64((uint64x2_t)cH[2 * i].val[1], (uint64x2_t)cH[2 * i + 1].val[1]);
     gH[i].val[1] = vzip1q_u64((uint64x2_t)cH[2 * i].val[1], (uint64x2_t)cH[2 * i + 1].val[1]);
   }
-  PERMUTE0(M[0], dL[0], dL[1], 0x20);
-  PERMUTE1(M[8], dL[0], dL[1], 0x31);
-  PERMUTE0(M[1], dH[0], dH[1], 0x20);
-  PERMUTE1(M[9], dH[0], dH[1], 0x31);
+  permute0(&M[16*6], gL[0], gL[1], 0x20);
+  permute1(&M[16*14], gL[0], gL[1], 0x31);
+  permute0(&M[16*7], gH[0], gH[1], 0x20);
+  permute1(&M[16*15], gH[0], gH[1], 0x31);
 
-  PERMUTE0(M[2], eL[0], eL[1], 0x20);
-  PERMUTE1(M[10], eL[0], eL[1], 0x31);
-  PERMUTE0(M[3], eH[0], eH[1], 0x20);
-  PERMUTE1(M[11], eH[0], eH[1], 0x31);
-
-  PERMUTE0(M[4], fL[0], fL[1], 0x20);
-  PERMUTE1(M[12], fL[0], fL[1], 0x31);
-  PERMUTE0(M[5], fH[0], fH[1], 0x20);
-  PERMUTE1(M[13], fH[0], fH[1], 0x31);
-
-  PERMUTE0(M[6], gL[0], gL[1], 0x20);
-  PERMUTE1(M[14], gL[0], gL[1], 0x31);
-  PERMUTE0(M[7], gH[0], gH[1], 0x20);
-  PERMUTE1(M[15], gH[0], gH[1], 0x31);
 }
