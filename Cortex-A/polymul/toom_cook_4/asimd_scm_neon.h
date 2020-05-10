f12733ac74f1b0b67c8bbeb5384e364cbe265b0f
cothan <cothannguyen@gmail.com>
2020-05-09 20:56:32 -0400

    rename c to c_mem, get restrict back

--- Cortex-A/polymul/toom_cook_4/asimd_scm_neon.h
+++ Cortex-A/polymul/toom_cook_4/asimd_scm_neon.h
@@ -2,7 +2,7 @@
 #define ASIMD_SCM_NEON 
 
 
-// void schoolbook_neon_new(uint16_t *restrict c, uint16_t *restrict a_mem, uint16_t *restrict b_mem);
-void schoolbook_neon_new(uint16_t *c, uint16_t *a_mem, uint16_t *b_mem);
+void schoolbook_neon_new(uint16_t *restrict c, uint16_t *restrict a_mem, uint16_t *restrict b_mem);
+// void schoolbook_neon_new(uint16_t *c, uint16_t *a_mem, uint16_t *b_mem);
 
 #endif
