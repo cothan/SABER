bba7f495f4778b93b656b13ed6920b2887b2de6f
cothan <cothannguyen@gmail.com>
2020-05-10 17:17:14 -0400

    fix

--- Cortex-A/polymul/toom_cook_4/asimd_scm_neon.c
+++ Cortex-A/polymul/toom_cook_4/asimd_scm_neon.c
@@ -8,10 +8,10 @@
 #define vmla(c, aa, bb) c = vmlaq_u16(c, aa, bb);
 
 // load c <= a 
-#define vload(c, a) c = vld1q_u16_x2(a);
+#define vload(c, a) c = vld1q_u16(a);
 
 // store c <= a 
-#define vstore(c, a) vst1q_u16_x2(c, a);
+#define vstore(c, a) vst1q_u16(c, a);
 
 // c = aa ^ bb 
 #define vxor(c, aa, bb) c = veorq_u16(aa, bb);
