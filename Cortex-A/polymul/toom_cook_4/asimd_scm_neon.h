36722f140aad1b16ba7209dee106d786cd732e80
cothan <cothannguyen@gmail.com>
2020-05-09 22:17:09 -0400

    clean up, better pipelining

--- Cortex-A/polymul/toom_cook_4/asimd_scm_neon.h
+++ Cortex-A/polymul/toom_cook_4/asimd_scm_neon.h
@@ -3,6 +3,5 @@
 
 
 void schoolbook_neon_new(uint16_t *restrict c, uint16_t *restrict a_mem, uint16_t *restrict b_mem);
-// void schoolbook_neon_new(uint16_t *c, uint16_t *a_mem, uint16_t *b_mem);
 
 #endif
