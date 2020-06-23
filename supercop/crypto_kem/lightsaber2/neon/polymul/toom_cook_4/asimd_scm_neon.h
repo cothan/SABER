f188ab305bd2a1b9885cc179c11b6340c66121de
cothan <cothannguyen@gmail.com>
2020-06-22 23:14:56 -0400

    lightsaber2 supercop

---
+++ supercop/crypto_kem/lightsaber2/neon/polymul/toom_cook_4/asimd_scm_neon.h
@@ -0,0 +1,7 @@
+#ifndef ASIMD_SCM_NEON
+#define ASIMD_SCM_NEON 
+
+
+void schoolbook_neon_new(uint16_t *restrict c, uint16_t *restrict a_mem, uint16_t *restrict b_mem);
+
+#endif
---
+++ supercop/crypto_kem/lightsaber2/neon/polymul/toom_cook_4/batch_64coefficient_multiplications.h
@@ -0,0 +1,18 @@
+#ifndef BATCH_64COEFFICIENT_MULTIPLICATIONS_H
+#define BATCH_64COEFFICIENT_MULTIPLICATIONS_H
+
+#include <arm_neon.h>
+#include "../../SABER_indcpa.h"
+#include "../../SABER_params.h"
+#include "asimd_scm_neon.h"
+
+void batch_64coefficient_multiplications(
+    uint16_t *restrict a0, uint16_t *restrict b0, uint16_t *restrict result_final0, 
+    uint16_t *restrict a1, uint16_t *restrict b1, uint16_t *restrict result_final1, 
+    uint16_t *restrict a2, uint16_t *restrict b2, uint16_t *restrict result_final2, 
+    uint16_t *restrict a3, uint16_t *restrict b3, uint16_t *restrict result_final3,
+    uint16_t *restrict a4, uint16_t *restrict b4, uint16_t *restrict result_final4, 
+    uint16_t *restrict a5, uint16_t *restrict b5, uint16_t *restrict result_final5, 
+    uint16_t *restrict a6, uint16_t *restrict b6, uint16_t *restrict result_final6);
+
+#endif
