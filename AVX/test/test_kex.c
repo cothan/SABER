6d0e45bc8171e8e7a708444b77c30481f2702630
cothan <cothannguyen@gmail.com>
2020-07-23 16:04:03 -0400

    rebase

--- AVX/test/test_kex.c
+++ AVX/test/test_kex.c
@@ -16,7 +16,7 @@
 #include<immintrin.h>
 #include<string.h>
 
-
+#include <papi.h>
 
 
 extern int crypto_kem_keypair(unsigned char *pk, unsigned char *sk);
@@ -35,7 +35,11 @@ int test_kem_cca()
   unsigned char entropy_input[48];
 	
   uint64_t i, j, repeat;
+<<<<<<< HEAD
   repeat=100000;
+=======
+  repeat=10000;
+>>>>>>> bd6e1e9... add test speed using papi only
   //repeat = 1;
 
   uint64_t CLOCK1,CLOCK2;
@@ -123,7 +127,68 @@ int test_kem_cca()
   	return 0;
 }
 
+int speed_test_kem_cca()
+{
 
+    uint8_t pk[SABER_PUBLICKEYBYTES];
+    uint8_t sk[SABER_SECRETKEYBYTES];
+    uint8_t c[SABER_BYTES_CCA_DEC];
+    uint8_t k_a[SABER_KEYBYTES], k_b[SABER_KEYBYTES];
+
+    int retval;
+
+    unsigned char entropy_input[48];
+
+    uint64_t i, repeat;
+    repeat = 10000;
+    //repeat = 1;
+
+    time_t t;
+    // Intializes random number generator
+    srand((unsigned)time(&t));
+
+    for (i = 0; i < 48; i++)
+    {
+        entropy_input[i] = i;
+        //entropy_input[i] = rand()%256;
+    }
+    randombytes_init(entropy_input, NULL, 256);
+    /* =================================== */
+    retval = PAPI_hl_region_begin("keypair");
+    for (i = 0; i < repeat; i++)
+    {
+        crypto_kem_keypair(pk, sk);
+    }
+    retval = PAPI_hl_region_end("keypair");
+    if (retval != PAPI_OK)
+    {
+        return 1;
+    }
+    /* =================================== */
+    retval = PAPI_hl_region_begin("encaps");
+    for (i = 0; i < repeat; i++)
+    {
+        crypto_kem_enc(c, k_a, pk);
+    }
+    retval = PAPI_hl_region_end("encaps");
+    if (retval != PAPI_OK)
+    {
+        return 1;
+    }
+    /* =================================== */
+    retval = PAPI_hl_region_begin("decaps");
+    for (i = 0; i < repeat; i++)
+    {
+        crypto_kem_dec(k_b, c, sk);
+    }
+    retval = PAPI_hl_region_end("decaps");
+    if (retval != PAPI_OK)
+    {
+        return 1;
+    }
+    /* =================================== */
+    return 0;
+}
 
 int main()
 {
@@ -131,6 +196,7 @@ int main()
 	//test_poly_mul();
 	//test_poly_sampling();
 	//test_sample_matrix();
-	test_kem_cca();
+	// test_kem_cca();
+	speed_test_kem_cca();
 	return 0;
 }
