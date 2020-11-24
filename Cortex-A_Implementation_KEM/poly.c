3ea5183fbde2ab633d355de891bd9268952f72c6
cothan <cothannguyen@gmail.com>
2020-11-23 23:16:02 -0500

    clean up

--- Cortex-A_Implementation_KEM/poly.c
+++ Cortex-A_Implementation_KEM/poly.c
@@ -9,38 +9,9 @@ Vadim Lyubashevsky, John M. Schanck, Peter Schwabe & Damien stehle
 #include <stdio.h>
 #include "api.h"
 #include "poly.h"
-#include "poly_mul.h"
 #include "pack_unpack.h"
 #include "cbd.h"
 #include "fips202.h"
-
-void MatrixVectorMul(const uint16_t A[SABER_L][SABER_L][SABER_N], const uint16_t s[SABER_L][SABER_N], uint16_t res[SABER_L][SABER_N], int16_t transpose)
-{
-	int i, j;
-	for (i = 0; i < SABER_L; i++)
-	{
-		for (j = 0; j < SABER_L; j++)
-		{
-			if (transpose == 1)
-			{
-				poly_mul_acc(A[j][i], s[j], res[i]);
-			}
-			else
-			{
-				poly_mul_acc(A[i][j], s[j], res[i]);
-			}	
-		}
-	}
-}
-
-void InnerProd(const uint16_t b[SABER_L][SABER_N], const uint16_t s[SABER_L][SABER_N], uint16_t res[SABER_N])
-{
-	int j;
-	for (j = 0; j < SABER_L; j++)
-	{
-		poly_mul_acc(b[j], s[j], res);
-	}
-}
 
 void GenMatrix(uint16_t A[SABER_L][SABER_L][SABER_N], const uint8_t seed[SABER_SEEDBYTES])
 {
--- Cortex-A_Implementation_KEM/poly.h
+++ Cortex-A_Implementation_KEM/poly.h
@@ -12,8 +12,6 @@ Vadim Lyubashevsky, John M. Schanck, Peter Schwabe & Damien stehle
 #include <stdint.h>
 #include "SABER_params.h"
 
-void MatrixVectorMul(const uint16_t a[SABER_L][SABER_L][SABER_N], const uint16_t s[SABER_L][SABER_N], uint16_t res[SABER_L][SABER_N], int16_t transpose);
-void InnerProd(const uint16_t b[SABER_L][SABER_N], const uint16_t s[SABER_L][SABER_N], uint16_t res[SABER_N]);
 void GenMatrix(uint16_t a[SABER_L][SABER_L][SABER_N], const uint8_t seed[SABER_SEEDBYTES]);
 void GenSecret(uint16_t s[SABER_L][SABER_N], const uint8_t seed[SABER_NOISE_SEEDBYTES]);
 
